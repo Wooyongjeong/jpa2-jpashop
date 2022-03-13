4718e596a9bc5325bbbac57bed4919338988c98b
wooyongjeong <wooyong.dev@gmail.com>
2022-03-13 14:38:40 +0900

    [API 개발 고급 - 컬렉션 조회 최적화] 주문 조회 V3: 엔티티를 DTO로 변환 - 페치 조인 최적화

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -98,4 +98,16 @@ public class OrderApiController {
         }
     }
 
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+
+        for (Order order : orders) {
+            System.out.println("order ref = " + order + ", id = " + order.getId());
+        }
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
 }
