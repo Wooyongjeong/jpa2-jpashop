ed7dda94fc8d4070527520a8c9b7155d43da98ca
wooyongjeong <wooyong.dev@gmail.com>
2022-03-30 16:58:53 +0900

    [API 개발 고급 - 컬렉션 조회 최적화] 주문 조회 V3.1: 엔티티를 DTO로 변환 - 페이징과 한계 돌파

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -9,6 +9,7 @@ import jpabook.jpashop.repository.OrderSearch;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -20,20 +21,17 @@ import java.util.stream.Collectors;
  * - 엔티티가 변하면 API 스펙이 변한다.
  * - 트랜잭션 안에서 지연 로딩 필요
  * - 양방향 연관관계 문제
- *
  * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
  * - 트랜잭션 안에서 지연 로딩 필요
  * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
  * - 페이징 시에는 N 부분을 포기해야함(대신에 batch fetch size? 옵션 주면 N -> 1 쿼리로 변경
- 가능)
- *
+ * 가능)
  * V4. JPA에서 DTO로 바로 조회, 컬렉션 N 조회 (1 + N Query)
  * - 페이징 가능
  * V5. JPA에서 DTO로 바로 조회, 컬렉션 1 조회 최적화 버전 (1 + 1 Query)
  * - 페이징 가능
  * V6. JPA에서 DTO로 바로 조회, 플랫 데이터(1Query) (1 Query)
  * - 페이징 불가능...
- *
  */
 @RestController
 @RequiredArgsConstructor
@@ -60,6 +58,32 @@ public class OrderApiController {
                 .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
         return collect;
+    }
+
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+
+        for (Order order : orders) {
+            System.out.println("order ref = " + order + ", id = " + order.getId());
+        }
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
     }
 
     @Data
@@ -98,16 +122,4 @@ public class OrderApiController {
         }
     }
 
-    @GetMapping("/api/v3/orders")
-    public List<OrderDto> ordersV3() {
-        List<Order> orders = orderRepository.findAllWithItem();
-
-        for (Order order : orders) {
-            System.out.println("order ref = " + order + ", id = " + order.getId());
-        }
-        List<OrderDto> result = orders.stream()
-                .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
-        return result;
-    }
 }
